5ad2c4b17a0a8425c8dc49e575f7db33ce025a63
KAKA12-19 <evenezertadesse@gmail.com>
2025-05-20 16:49:15 +0300

    some update in viteConf

--- nitflix/vite.config.js
+++ nitflix/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Netflix-By-Evenezer/", // Must match your repo name exactly
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+  },
+});
